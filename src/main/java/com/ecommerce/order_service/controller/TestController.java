53cd8e7a18cdd84f38e1eed28449d2fa4f852578
Anuradha Prabhath Kadurugasyaya <prabhath@Anuradhas-MacBook-Pro.local>
2025-02-11 09:17:44 +0800

    Added sl4j logger

--- src/main/java/com/ecommerce/order_service/controller/TestController.java
+++ src/main/java/com/ecommerce/order_service/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.ecommerce.order_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
+
 }
